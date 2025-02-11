02198a9bb058224bbeca49f0a197716cbffdda52
LofiSu <linsu269@gmail.com>
2025-02-11 13:57:43 +0800

    fix: fix name

--- frontend/rsbuild.config.ts
+++ frontend/rsbuild.config.ts
@@ -3,10 +3,13 @@ import { defineConfig } from "@rsbuild/core";
 import { pluginReact } from "@rsbuild/plugin-react";
 
 export default defineConfig({
+  html: {
+    title: "Kawa-Face",
+  },
   plugins: [pluginReact()],
   source: {
     entry: {
-      index: './src/index.tsx',
+      index: "./src/index.tsx",
     },
   },
 });
